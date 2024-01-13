07046dc9924399728164fc6b0206705ed7302c6e
Nqvinh7603 <nqvinh7603@gmail.com>
2024-01-13 13:42:16 +0700

    update final code front-end

---
+++ .history/src/todo/TodoApp_20240113132916.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./TodoApp.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LogoutComponent from "./component/LogoutComponent";
+import FooterComponent from "./component/FooterComponent";
+import ListTodosComponent from "./component/ListTodosComponent";
+import HeaderComponent from "./component/HeaderComponent";
+import ErrorComponent from "./component/ErrorComponent";
+import WelcomeComponent from "./component/WelcomeComponent";
+import LoginComponent from "./component/LoginComponent";
+import AuthProvider from "./security/AuthContext";
+function AuthencicateRoute(){
+  return(
+
+  )
+}
+const TodoApp = () => {
+  return (
+    <div className="TodoApp">
+    <AuthProvider>
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          <Route path="/" element={<LoginComponent />} />
+          <Route path="/login" element={<LoginComponent />} />
+          <Route path="/welcome/:username" element={
+            <AuthencicateRoute></AuthencicateRoute>
+          <WelcomeComponent />} />
+          <Route path="/todos" element={<ListTodosComponent />} />
+          <Route path="/logout" element={<LogoutComponent />} />
+          <Route path="*" element={<ErrorComponent />} />
+        </Routes>
+        
+      </BrowserRouter>
+      </AuthProvider>
+    </div>
+  );
+};
+
+export default TodoApp;
---
+++ .history/src/todo/TodoApp_20240113133206.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./TodoApp.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LogoutComponent from "./component/LogoutComponent";
+import ListTodosComponent from "./component/ListTodosComponent";
+import HeaderComponent from "./component/HeaderComponent";
+import ErrorComponent from "./component/ErrorComponent";
+import WelcomeComponent from "./component/WelcomeComponent";
+import LoginComponent from "./component/LoginComponent";
+import AuthProvider, { useAuth } from "./security/AuthContext";
+function AuthencicateRoute({children}){
+  const authContext = useAuth();
+  if(authContext.is)
+  return children;
+}
+const TodoApp = () => {
+  return (
+    <div className="TodoApp">
+    <AuthProvider>
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          <Route path="/" element={<LoginComponent />} />
+          <Route path="/login" element={<LoginComponent />} />
+          <Route path="/welcome/:username" element={
+            <AuthencicateRoute><WelcomeComponent/></AuthencicateRoute>
+          } />
+          <Route path="/todos" element={<ListTodosComponent />} />
+          <Route path="/logout" element={<LogoutComponent />} />
+          <Route path="*" element={<ErrorComponent />} />
+        </Routes>
+        
+      </BrowserRouter>
+      </AuthProvider>
+    </div>
+  );
+};
+
+export default TodoApp;
